5a4e73d2865db21a420ec2c66edc64abed77d68f
NaehaSharif <47408307+NaehaSharif@users.noreply.github.com>
2024-02-14 16:29:03 +0800

    Delete Labs/lab2-soln/lab2-soln directory

--- Labs/lab2-soln/lab2-soln/LINUX_VERSIONS/vshader.glsl
+++
@@ -1,8 +0,0 @@
-#version 150
-
-in vec3 vPosition;
-
-void main()
-{
-    gl_Position = vec4(vPosition, 1.0);
-}
--- Labs/lab2-soln/lab2-soln/MAC_VERSIONS/vshader.glsl
+++
@@ -1,6 +0,0 @@
-attribute vec3 vPosition;
-
-void main()
-{
-    gl_Position = vec4(vPosition, 1.0);
-}
